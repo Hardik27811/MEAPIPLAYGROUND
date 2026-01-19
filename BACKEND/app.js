e12d973dc6904c59192b298345496916449683a3
Hardik Sharma <kumarankit2002dseu@gmail.com>
2026-01-19 10:24:12 +0530

    changed cors

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -9,9 +9,25 @@ const app = express();
 
 app.use(cookieParser());
 
+app.set("trust proxy", 1);
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://meapiplayground-ten.vercel.app"
+];
+
 app.use(
   cors({
-    origin: process.env.VERCEL_URL ,
+    origin: function (origin, callback) {
+
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("CORS not allowed"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
